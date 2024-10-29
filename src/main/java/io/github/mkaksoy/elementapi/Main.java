f52913854f728636fd629b8b1112b57568338dc2
Mehmet Kaan Aksoy <mehmetkaanaksoy13@gmail.com>
2024-10-29 22:46:06 +0300

    Update version to 0.0.2: Added Paper dependency, created Main class, and added plugin.yml.

--- src/main/java/io/github/mkaksoy/elementapi/Main.java
+++ src/main/java/io/github/mkaksoy/elementapi/Main.java
@@ -1,7 +1,15 @@
 package io.github.mkaksoy.elementapi;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.print("Coming soon!");
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("ElementAPI 0.0.2 has been enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("ElementAPI 0.0.2 has been disabled!");
     }
 }
